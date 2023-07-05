364706dd119fe480d2f2b46dec2fe5f0ca1aebc2
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-05 17:46:33 +0500

    calling navbar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
           {children}
       </body>
     </html>
