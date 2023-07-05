f8285123c436901d4c518e0b94e33b84c9524b22
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-05 18:15:33 +0500

    female section route created

---
+++ src/app/female/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductCard from '@/components/ProductCard';
+
+const page = () => {
+  return (
+    <main>
+      <div className='grid grid-cols-4 justify-items-center gap-16 my-16 mx-32'>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </main>
+  )
+}
+
+export default page
