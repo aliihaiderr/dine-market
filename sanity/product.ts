676c3f65ebf558f403e0e75343b8bd7eaa788775
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-06 15:45:03 +0500

    product schema updated

--- sanity/product.ts
+++ sanity/product.ts
@@ -9,15 +9,65 @@ const product = {
             title: 'Product Name'
         },
         {
-            name: 'description',
+            name: 'type',
             type: 'string',
+            title: 'Product Type',
+            options: {
+                list: [
+                  {title: 'Sweater', value: 'sweater'},
+                  {title: 'Dress', value: 'dress'},
+                  {title: 'Pant', value: 'pant'},
+                  {title: 'Jacket', value: 'jacket'},
+                  {title: 'T Shirt', value: 't shirt'},
+                ],
+                layout: 'radio'
+              }
+        },
+        {
+            name: 'category',
+            type: 'string',
+            title: 'Product Category',
+            options: {
+                list: [
+                  {title: 'Women', value: 'women'},
+                  {title: 'Men', value: 'men'},
+                  {title: 'Kids', value: 'kids'},
+                ],
+                layout: 'radio'
+              }
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Product Price'
+        },
+        {
+            name: 'description',
+            type: 'text',
             title: 'Product Description'
+        },
+        {
+            name: 'care',
+            type: 'array',
+            title: 'Product Care',
+            of : [{         
+                type : 'string'
+            }],
+            options: {
+                list: [
+                    {title: 'Hand wash using cold water.', value: 'opt1'},
+                    {title: 'Do not using bleach.', value: 'opt2'},
+                    {title: 'Hang it to dry.', value: 'opt3'},
+                    {title: 'Iron on low temperature.', value: 'opt4'},
+                ]
+              }
         },
         {
             name: 'image',
             type: 'array',
             title: 'Product Image',
-            of : [{         // we use of for upload multiple images and datatype is array
+            // The of property specifies which value types the array may hold.
+            of : [{         
                 name : 'img',
                 title : 'Image',
                 type : 'image'
