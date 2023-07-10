1f4c1593d0103ebe389a0d2dfcbd5f6457fd0200
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-11 00:08:00 +0500

    configure client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,13 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+// client is used as a bridge between sanity and next.js to access, read and edit data.
+
+// import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
+  apiVersion : "v2023-07-04",
+  dataset : process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  token : process.env.SANITY_ACCESS_TOKEN,
+  useCdn : false,
 })
