760e27a00dfc1f8d763758c830179ec5d777b743
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-11 12:06:33 +0500

    male and female products fetched from sanity

--- src/app/female/page.tsx
+++ src/app/female/page.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import ProductCard from '@/components/ProductCard';
+import Image from 'next/image';
+import {client} from '../../../sanity/lib/client';
+import { Image as IImage } from 'sanity';
+import { urlForImage } from '../../../sanity/lib/image';
 
-const page = () => {
+export const getProductData = async () => {
+ const response = await client.fetch(`*[_type=="product" && category->name=='Female']{name,type,price,image[]}`);
+ return response;
+}
+
+interface Product {
+  name : string,
+  type : string,
+  price : number,
+  image : IImage[],
+}
+
+export default async function page() {
+  const data :Product[]  = await getProductData();
   return (
     <main>
       <div className='grid grid-cols-4 justify-items-center gap-16 my-16 mx-32'>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {data.map((item) =>(
+            <div key={item.name}>
+              <a href='/detail'>
+                <div>
+                  <Image src={urlForImage(item.image[0]).url()} alt='productImg' height={270} width={250} />
+                  <p className='text-[1.05rem] leading-6 mt-2 font-semibold tracking-wide text-[#212121]'>{item.name}</p>
+                  <p className='text-base mt-1 font-semibold text-[#888]'>{item.type}</p>
+                  <p className='text-xl mt-3 font-semibold tracking-wide text-[#212121]'>${item.price}</p>
+                </div>
+              </a>
+            </div>
+        ))}
       </div>
     </main>
   )
 }
-
-export default page
