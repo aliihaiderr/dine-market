36291482310ef12ed39b8e23fda72c29ca7f280f
aliihaiderr <alihaidar.dev@gmail.com>
2023-07-14 21:13:18 +0500

    minor update

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import {client} from '../../../sanity/lib/client';
 import { Image as IImage } from 'sanity';
 import { urlForImage } from '../../../sanity/lib/image';
+import Link from 'next/link';
+import product from '../../../sanity/product';
 
 
   
@@ -12,11 +14,12 @@ import { urlForImage } from '../../../sanity/lib/image';
 
 
 export const getProductData = async () => {
- const response = await client.fetch(`*[ _type == 'product']{name,type,price,image[]}`);
+ const response = await client.fetch(`*[ _type == 'product']{_id,name,type,price,image[]}`);
  return response;
 }
 
 interface Product {
+  _id : string,
   name : string,
   type : string,
   price : number,
@@ -29,15 +32,15 @@ export default async function page() {
     <main>
       <div className='grid grid-cols-4 justify-items-center gap-16 my-16 mx-32'>
         {data.map((item) =>(
-            <div key={item.name}>
-              <a href='/detail'>
+            <div key={item._id}>
+              <Link href={`/products/${item._id}`}>
                 <div>
                   <Image src={urlForImage(item.image[0]).url()} alt='productImg' height={270} width={250} />
                   <p className='text-[1.05rem] leading-6 mt-2 font-semibold tracking-wide text-[#212121]'>{item.name}</p>
                   <p className='text-base mt-1 font-semibold text-[#888]'>{item.type}</p>
                   <p className='text-xl mt-3 font-semibold tracking-wide text-[#212121]'>${item.price}</p>
                 </div>
-              </a>
+              </Link>
             </div>
         ))}
       </div>
